d89894bf2924050e6ac33adf3028c75edc935386
adityasripalle2 <124156906+adityasripalle2@users.noreply.github.com>
2024-12-11 06:51:07 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -17,4 +17,29 @@ public class Test{
      }//end of else  
    }    
    }   
+
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpServer;
+
+//9
+
+public class Test {
+    public static void main(String[] args) throws IOException 
+         {
+        int port = 8080; // Port on which the server will listen
+        System.out.println("Starting server on port " + port);
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/", (exchange -> {
+            String response = "Hello, Kubernetes!";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            exchange.getResponseBody().write(response.getBytes());
+            exchange.close();
+        }));
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server is running at http://localhost:" + port);
+    }
+}
    
