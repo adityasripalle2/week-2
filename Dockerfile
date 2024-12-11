345faf9c921dbb7fb95fb8c219b70f20d46d88b7
adityasripalle2 <124156906+adityasripalle2@users.noreply.github.com>
2024-12-11 06:49:16 +0530

    basic
    ee

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,5 @@
-# Use OpenJDK base image
-FROM openjdk:17-alpine
-# Set working directory
-WORKDIR /app
-# Copy all files to the working directory
-COPY . /app
-# Compile the Java application
+FROM openjdk
+WORKDIR  /sam
+COPY  . /sam
 RUN javac Test.java
-# Expose port 8080 for the application
-EXPOSE 8080
-# Run the Java application
-CMD ["java", "Test"]
+CMD ["java","Test"]
--- Test.java
+++ Test.java
@@ -1,23 +1,20 @@
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import com.sun.net.httpserver.HttpServer;
-
-
-
-public class Test {
-    public static void main(String[] args) throws IOException 
-         {
-        int port = 8080; // Port on which the server will listen
-        System.out.println("Starting server on port " + port);
-        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-        server.createContext("/", (exchange -> {
-            String response = "Hello, Kubernetes!";
-            exchange.sendResponseHeaders(200, response.getBytes().length);
-            exchange.getResponseBody().write(response.getBytes());
-            exchange.close();
-        }));
-        server.setExecutor(null);
-        server.start();
-        System.out.println("Server is running at http://localhost:" + port);
-    }
-}
+public class Test{    
+    public static void main(String args[]){    
+     int i,m=0,flag=0;      
+     int n=3;//it is the number to be checked    
+     m=n/2;      
+     if(n==0||n==1){  
+      System.out.println(n+" is not prime number");      
+     }else{  
+      for(i=2;i<=m;i++){      
+       if(n%i==0){      
+        System.out.println(n+" is not prime number");      
+        flag=1;      
+        break;      
+       }      
+      }      
+      if(flag==0)  { System.out.println(n+" is prime number"); }  
+     }//end of else  
+   }    
+   }   
+   
